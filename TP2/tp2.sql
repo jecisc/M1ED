726c814f33bf332a029308ba06db50a96847ac9c
Pinguu <rousseau.aurelien@hotmail.fr>
2015-11-18 19:40:23 +0100

    Ex1 fini BITCH

--- TP2/tp2.sql
+++ TP2/tp2.sql
@@ -1,3 +1,14 @@
+DROP materialized VIEW log on client;
+DROP materialized VIEW log on facture;
+DROP materialized VIEW log on produit;
+DROP materialized VIEW log on ligne_facture;
+
+DROP materialized VIEW produit_dim;
+DROP materialized VIEW lieu_dim;
+DROP materialized VIEW client_dim;
+DROP materialized VIEW temps_dim;
+DROP materialized VIEW fait_vente;
+
 -- pour créer une vue materialisee avec la clause "refresh fast",
 -- il faut d'abord créer un log sur la table de base
 
@@ -26,11 +37,11 @@ create materialized view log on client;
 create materialized view client_dim
 refresh force on demand
 as select num as id_client,
-floor(months_between((sysdate, datenaiss)/12)) as age,
+floor(months_between(sysdate, date_nais)/12) as age,
 case 
-when floor(months_between((sysdate, datenaiss)/12)) < 30 then '<30'
-when floor(months_between((sysdate, datenaiss)/12)) < 45 then '30-44'
-when floor(months_between((sysdate, datenaiss)/12)) < 60 then '45-59'
+when floor(months_between(sysdate, date_nais)/12) < 30 then '<30'
+when floor(months_between(sysdate, date_nais)/12) < 45 then '30-44'
+when floor(months_between(sysdate, date_nais)/12) < 60 then '45-59'
 else '>=60'
 end as tranche_age,
 upper(substr(sexe, 1, 1)) as sexe
@@ -55,6 +66,20 @@ TO_CHAR(facture.date_etabli, 'ddd') as jour_annee,
 TO_CHAR(facture.date_etabli, 'ww') as semaine_annee,
 TO_CHAR(facture.date_etabli, 'dy') as lib_jour
 from facture;
+
+--
+-- Dimension lieu
+--
+
+create materialized view lieu_dim
+refresh force on demand
+as select
+     regexp_substr(adresse,'[^,]+',1,2) AS zip_code,
+     regexp_substr(adresse,'[^,]+',1,3) AS ville,
+     regexp_substr(adresse,'[^,]+',1,4) AS pays 
+from client;
+
+
 --
 -- Fait
 --
@@ -68,7 +93,7 @@ ligne_facture.facture as id_facture,
 facture.date_etabli as date_vente,
 ligne_facture.produit as id_produit,
 facture.client as id_client,
-regexp_substr(client.adresse, '[^.]+', 1, 2) || '_' || regexp_substr(client.adresse, '[^.]+', 2, 3) as id_adresse,
+regexp_substr(client.adresse, '[^,]+', 1, 2) || '_' || regexp_substr(client.adresse, '[^,]+', 1, 3) as id_adresse,
 prix_date.remise as remise,
 ligne_facture.qte as qte,
 prix_date.prix as prix_unit,
@@ -78,14 +103,7 @@ join facture on ligne_facture.facture = facture.num
 join prix_date on ligne_facture.id_prix = prix_date.num
 join client on facture.client = client.num;
 
-insert into ligne_facture (facture, produit, qte, id_prix) values (3,5,10,5);
-insert into ligne_facture (facture, produit, qte, id_prix) values (45,2,10,2);
-insert into ligne_facture (facture, produit, qte, id_prix) values (80,11,12,11);
+-- Question implementations
 
-
-
-SELECT TO_CHAR(date_etabli, 'ww') as d
-from FACTURE
-;
-select now();
-
+alter materialized view fait_vente
+add constraint  pk_fait_vente primary key (id_facture);
